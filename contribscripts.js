6ae8a050a7783fb99b4486056889bd20516bf726
trygvanwyk <63888704+trygvanwyk@users.noreply.github.com>
2020-05-04 21:53:37 -0500

    Updated photo size

--- contribscripts.js
+++ contribscripts.js
@@ -9,5 +9,5 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 4000); // Change image every 2 seconds
+  setTimeout(carousel, 3000); // Change image every 2 seconds
 }
