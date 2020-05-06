353efc4ad85d78a24d1f39602b2fed23082521d7
trygvanwyk <63888704+trygvanwyk@users.noreply.github.com>
2020-05-06 11:54:25 -0500

    Text slideshow attempt

--- contribscripts.js
+++ contribscripts.js
@@ -9,5 +9,19 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 5000); // Change image every 2 seconds
+  setTimeout(carousel, 5000); // set duration in ms
+}
+
+var slideIndex = 0;
+
+function textCarousel() {
+  var i;
+  var x = document.getElementsByClassName("myParas");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 5000); // set duration in ms
 }
