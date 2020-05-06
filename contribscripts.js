847572cb63c8669ee674631b893b2c7e5a3f7e8f
trygvanwyk <63888704+trygvanwyk@users.noreply.github.com>
2020-05-06 13:19:23 -0500

    Cover text update

--- contribscripts.js
+++ contribscripts.js
@@ -16,11 +16,11 @@ var slideIndex = 0;
 
 function textCarousel() {
   var i;
-  var x = document.getElementsByClassName("myParas");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  var x = document.getElementsByClassName("coverParagraph");
+  for (i = 0; q < x.length; i++) {
+    x[q].style.display = "none";
   }
-  slideIndex++;
+  slideIndex1++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 5000); // set duration in ms
