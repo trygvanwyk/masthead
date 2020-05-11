3251d3a3d9b3c876f76b23745924d9928b54db76
trygvanwyk <63888704+trygvanwyk@users.noreply.github.com>
2020-05-10 23:12:17 -0500

    News cards js completed and content outlined

--- contribscripts.js
+++ contribscripts.js
@@ -24,3 +24,32 @@ function carousel() {
   y[slideIndex1-1].style.display = "block";
   setTimeout(carousel, 4000); // set duration in ms
 }
+
+var slideIndex2 = 1;
+showSlides(slideIndex2);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex2 = n);
+}
+
+function showSlides(n) {
+  var w;
+  var slides = document.getElementsByClassName("newsCard");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (w = 0; w < slides.length; w++) {
+      slides[w].style.display = "none";
+  }
+  for (w = 0; w < dots.length; w++) {
+      dots[w].className = dots[w].className.replace(" active", "");
+  }
+  slides[slideIndex2-1].style.display = "block";
+  dots[slideIndex2-1].className += " active";
+}
